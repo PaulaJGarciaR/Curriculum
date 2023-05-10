29bb74b13a71235792f24345a174ef494732ef97
PaulaJGarciaR <pjgarciar@ufpso.edu.co>
2023-05-10 17:05:58 -0500

    Aprediendo Js

---
+++ js/Clase.js
@@ -0,0 +1,34 @@
+//Variables
+const nameUser="PaulaGarcia";
+var x="Valor";
+let y=" Valor";
+
+let a=200;
+let b=2.4;
+let nombre="PaulaGarciaRodriguez";
+let boolean=true;
+let array =[1,2,3,4];
+let arrayDos=["Lunes","Martes","Miercoles","Jueves","Viernes"];
+console.log(arrayDos[1]);
+
+let suma=1+8+a;
+let resta=6-8+b;
+let potencia=6^6;
+let modulo =4%2;
+let multiplicacion=4*6;
+let division=9/3;
+
+console.log("resultado Suma:"+suma+"\n"+"Resultado Resta:"+resta);
+
+if(10>5){
+    for(let i=0;i<arrayDos.length;i++){
+        console.log(arrayDos[i]);
+    }
+}
+let j=5;
+let k=10;
+
+while(k>=j){
+    console.log("ENTRA");
+    k--;
+}
